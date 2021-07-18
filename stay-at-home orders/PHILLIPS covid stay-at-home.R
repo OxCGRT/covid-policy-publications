93beff6403108a07e3a08f35287466a250ca8350
Toby Phillips <59369581+TobyPhillips@users.noreply.github.com>
2021-07-18 18:53:59 +1000

    Update R script for revised figures

--- stay-at-home orders/PHILLIPS covid stay-at-home.R
+++ stay-at-home orders/PHILLIPS covid stay-at-home.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
+library(lubridate)
 library(httr)
 library(zoo)
+library(cowplot)
 
 
 # Get latest OxCGRT dataset
@@ -11,13 +13,15 @@ data <- GET("https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/maste
 
 national <- read.csv(textConnection(rawToChar(data$content))) %>%
   mutate(Date = ymd(Date)) %>%
-  filter(Date < "2021-03-01") %>%    # filter out data added after March 2021, when this was written
+  filter(Date < "2021-04-01") %>%    # filter out data added after March 2021, when this was written
   mutate(Date = as.Date(Date, format = "%Y-%m-%d")) %>%
   filter(Jurisdiction == "NAT_TOTAL") %>%    # remove subnational data
   group_by(Date)
 
 
-## FIG 1
+################
+##  FIGURE 1  ##
+################
 
 americas <- c("AIA", "ATG", "ABW", "BHS", "BRB", "BES", "VGB", "CYM", "CUB", "CUW", "DMA", "DOM", "GRD", "GLP", "HTI", "JAM", "MTQ", "MSR", "PRI", "BLM", "KNA", "LCA", "MAF", "VCT", "SXM", "TTO", "TCA", "VIR", "BLZ", "CRI", "SLV", "GTM", "HND", "MEX", "NIC", "PAN", "ARG", "BOL", "BVT", "BRA", "CHL", "COL", "ECU", "FLK", "GUF", "GUY", "PRY", "PER", "SGS", "SUR", "URY", "VEN", "BMU", "CAN", "GRL", "SPM", "USA", "ATA")
 europe <- c("BLR", "BGR", "CZE", "HUN", "POL", "MDA", "ROU", "RUS", "SVK", "UKR", "ALA", "GGY", "JEY", "DNK", "EST", "FRO", "FIN", "ISL", "IRL", "IMN", "LVA", "LTU", "NOR", "SJM", "SWE", "GBR", "ALB", "AND", "BIH", "HRV", "GIB", "GRC", "VAT", "ITA", "MLT", "MNE", "MKD", "PRT", "SMR", "SRB", "SVN", "ESP", "AUT", "BEL", "FRA", "DEU", "LIE", "LUX", "MCO", "NLD", "CHE", "RKS")
@@ -47,7 +51,10 @@ national %>%
     theme_bw()
 
 
-## FIG 2
+
+################
+##  FIGURE 2  ##
+################
 
 # add variables for 7-day average case counts, and marking the beggining and end of stay-at-home periods 
 national <- national %>%
@@ -61,7 +68,7 @@ national <- national %>%
   mutate(stay_home = C6_Stay.at.home.requirements >= 2) %>%
   ungroup() %>%
   arrange(CountryCode, Date) %>%
-  select(CountryCode, Date, StringencyIndex, C6_Stay.at.home.requirements, ave7day_newcases, stay_home) %>%
+  select(CountryCode, Date, StringencyIndex, StringencyIndexForDisplay, C6_Stay.at.home.requirements, ave7day_newcases, stay_home) %>%
   group_by(CountryCode) %>%
   mutate(start_date_bool = stay_home == TRUE & !lag(stay_home),
          end_date_bool = stay_home == TRUE & (!lead(stay_home, default = FALSE) | is.na(lead(stay_home)))) %>%
@@ -79,7 +86,7 @@ stay_home_periods <- national %>%
 
 # list countries that averaged over 100 cases per day between April 2020 - March 2021
 over100 <- national %>%
-  filter(Date > "2020-03-31") %>%
+  filter(Date > "2020-07-31") %>%
   group_by(CountryCode) %>%
   summarise(avecases = mean(ave7day_newcases)) %>%
   filter(avecases > 100) %>%
@@ -88,7 +95,7 @@ over100 <- national %>%
 
 # lsit countries that averaged under 100 cases per day between April 2020 - March 2021
 under100 <- national %>%
-  filter(Date > "2020-03-31") %>%
+  filter(Date > "2020-07-31") %>%
   group_by(CountryCode) %>%
   summarise(avecases = mean(ave7day_newcases)) %>%
   filter(avecases <= 100) %>%
@@ -115,7 +122,6 @@ C6_more_than_once <- stay_home_periods %>%
   select(CountryCode) %>%
   as_vector()
 
-# produce fig 2a
 fig_2_data <- national %>%
   filter(CountryCode %in% c(first_C6_10_cases_or_more),
          CountryCode %in% c(C6_more_than_once)) %>%
@@ -125,45 +131,114 @@ fig_2_data <- national %>%
   group_by(CountryCode) %>%
   mutate(relative_cases = start_cases / start_cases[1])
 
-ggplot(data = fig_2_data, mapping = aes(x = Date, y = relative_cases, color = under_100)) +
-  geom_point(show.legend=FALSE) +
-  scale_y_log10(labels = function(x) format(x, scientific = FALSE), n.breaks = 6) +
-  geom_text(data = filter(fig_2_data, Date == last(Date)), aes(label=CountryCode),hjust=0, vjust=1, show.legend=FALSE) +
-  geom_line(aes(group = CountryCode, linetype = under_100), alpha = 0.4, size = 0.8) +
-  labs(title = "(a)",
-       y = "Cases-per-day, relative to \ncases-per-day at first stay-at-home order" ,
-       color = "Average daily cases since April 2020") +
-  scale_colour_manual(name = "Average daily \ncases April 2020 \nto March 2021", values = c("FALSE" = "orangered3", "TRUE" = "dodgerblue"), labels = c("more than 100", "100 or less")) +
-  scale_linetype_manual(name = "Average daily \ncases April 2020 \nto March 2021", values = c("FALSE" = "dashed", "TRUE" = "solid"), labels = c("more than 100", "100 or less")) +
-  theme_bw()
-
-# produce fig 2b
+# produce fig 2a
 PHL_stay_home_periods <- stay_home_periods %>%
   filter(CountryCode == "PHL")
-national %>%
+fig2a <- national %>%
   filter(CountryCode == "PHL") %>%
   ggplot(aes(x = Date, y = ave7day_newcases, group = CountryCode, color = CountryCode)) +
   annotate(geom = "rect", xmin = PHL_stay_home_periods$start_date, xmax = PHL_stay_home_periods$end_date, ymin = 0, ymax = Inf, fill = "red", alpha = 0.4) +
   geom_line(color = "black") +
   geom_point(aes(x = Date, y = start_cases), color = "dodgerblue", size = 3) +
   annotate(geom = "segment", x = as.Date("2020-01-01", format = "%Y-%m-%d"), xend = PHL_stay_home_periods$start_date, y = PHL_stay_home_periods$start_cases, yend = PHL_stay_home_periods$start_cases, color = "dodgerblue", linetype = "dashed", size = 0.8) +
-  scale_y_log10(labels = function(x) format(x, scientific = FALSE)) +
-  labs(title = "(b) stay-at-home orders in the Philippines",
-       y = "New cases per day (7-day rolling average)") +
-  theme_bw()
+#  scale_y_log10(labels = function(x) format(x, scientific = FALSE)) +
+  scale_y_log10(labels = c("1", "10", "100", "1000", "10000", "100000"), breaks = c(1, 10, 100, 1000, 10000, 100000), limits = c(1, 100000)) +
+  labs(title = "(a) Philippines",
+       y = "New cases per day \n(7-day rolling average)") +
+  theme_bw() +
+  theme(plot.title = element_text(size=10))
+
+
+# produce fig 2b
+GBR_stay_home_periods <- stay_home_periods %>%
+  filter(CountryCode == "GBR")
+fig2b <- national %>%
+  filter(CountryCode == "GBR") %>%
+  ggplot(aes(x = Date, y = ave7day_newcases, group = CountryCode, color = CountryCode)) +
+  annotate(geom = "rect", xmin = GBR_stay_home_periods$start_date, xmax = GBR_stay_home_periods$end_date, ymin = 0, ymax = Inf, fill = "red", alpha = 0.4) +
+  geom_line(color = "black") +
+  geom_point(aes(x = Date, y = start_cases), color = "dodgerblue", size = 3) +
+  annotate(geom = "segment", x = as.Date("2020-01-01", format = "%Y-%m-%d"), xend = GBR_stay_home_periods$start_date, y = GBR_stay_home_periods$start_cases, yend = GBR_stay_home_periods$start_cases, color = "dodgerblue", linetype = "dashed", size = 0.8) +
+#  scale_y_log10(labels = function(x) format(x, scientific = FALSE)) +
+  scale_y_log10(labels = c("1", "10", "100", "1000", "10000", "100000"), breaks = c(1, 10, 100, 1000, 10000, 100000), limits = c(1, 100000)) +
+  labs(title = "(b) United Kingdom",
+       y = "New cases per day \n(7-day rolling average)") +
+  ylab(NULL) +
+  theme_bw() +
+  theme(plot.title = element_text(size=10))
 
 # produce fig 2c
 AUS_stay_home_periods <- stay_home_periods %>%
   filter(CountryCode == "AUS")
-national %>%
+fig2c <- national %>%
   filter(CountryCode == "AUS") %>%
   ggplot(aes(x = Date, y = ave7day_newcases, group = CountryCode, color = CountryCode)) +
     annotate(geom = "rect", xmin = AUS_stay_home_periods$start_date, xmax = AUS_stay_home_periods$end_date, ymin = 0, ymax = Inf, fill = "red", alpha = 0.4) +
     geom_line(color = "black") +
     geom_point(aes(x = Date, y = start_cases), color = "dodgerblue", size = 3) +
     annotate(geom = "segment", x = as.Date("2020-01-01", format = "%Y-%m-%d"), xend = AUS_stay_home_periods$start_date, y = AUS_stay_home_periods$start_cases, yend = AUS_stay_home_periods$start_cases, color = "dodgerblue", linetype = "dashed", size = 0.8) +
-    scale_y_log10(labels = function(x) format(x, scientific = FALSE)) +
-    labs(title = "(c) stay-at-home orders in Australia",
-       y = "New cases per day (7-day rolling average)") +
+#    scale_y_log10(labels = function(x) format(x, scientific = FALSE)) +
+    scale_y_log10(labels = c("1", "10", "100", "1000", "10000"), breaks = c(1, 10, 100, 1000, 10000), limits = c(1, 10000)) +
+    labs(title = "(c) Australia",
+       y = "New cases per day \n(7-day rolling average)") +
     theme(legend.position = "none") +
-    theme_bw()
+    theme_bw() +
+    theme(plot.title = element_text(size=10))
+
+
+# produce fig 2d
+CHN_stay_home_periods <- stay_home_periods %>%
+  filter(CountryCode == "CHN")
+fig2d <- national %>%
+  filter(CountryCode == "CHN") %>%
+  ggplot(aes(x = Date, y = ave7day_newcases, group = CountryCode, color = CountryCode)) +
+  annotate(geom = "rect", xmin = CHN_stay_home_periods$start_date, xmax = CHN_stay_home_periods$end_date, ymin = 0, ymax = Inf, fill = "red", alpha = 0.4) +
+  geom_line(color = "black") +
+  geom_point(aes(x = Date, y = start_cases), color = "dodgerblue", size = 3) +
+  annotate(geom = "segment", x = as.Date("2020-01-01", format = "%Y-%m-%d"), xend = CHN_stay_home_periods$start_date, y = CHN_stay_home_periods$start_cases, yend = CHN_stay_home_periods$start_cases, color = "dodgerblue", linetype = "dashed", size = 0.8) +
+#  scale_y_log10(labels = function(x) format(x, scientific = FALSE)) +
+  scale_y_log10(labels = c("1", "10", "100", "1000", "10000"), breaks = c(1, 10, 100, 1000, 10000), limits = c(1, 10000)) +
+  labs(title = "(d) China",
+       y = "New cases per day \n(7-day rolling average)") +
+  theme(legend.position = "none") +
+  ylab(NULL) +
+  theme_bw() +
+  theme(plot.title = element_text(size=10))
+
+plot_grid(fig2a, fig2b, fig2c, fig2d, labels = "", label_size = 10, rel_widths = c(1.1, 1))
+
+
+
+################
+##  FIGURE 3  ##
+################
+
+# produce fig 3a
+fig3a <- ggplot(data = filter(fig_2_data, over_100==TRUE), mapping = aes(x = Date, y = relative_cases, color = under_100)) +
+  geom_point(show.legend=FALSE) +
+  scale_y_log10(labels = c("0.01", "0.1", "1", "10", "100", "1000"), breaks = c(0.01, 0.1, 1, 10, 100, 1000), limits = c(0.0035, 800)) +
+  geom_text(data = filter(fig_2_data, over_100==TRUE, Date == last(Date)), aes(label=CountryCode),hjust=0, vjust=1, show.legend=FALSE, nudge_x = -25, nudge_y = 0.2, check_overlap = TRUE) +
+  geom_line(aes(group = CountryCode, linetype = over_100), alpha = 0.4, size = 0.8, show.legend=FALSE) +
+  labs(
+    #    title = "(a)",
+    #    color = "Average daily cases since April 2020",
+    y = "Cases-per-day, relative to \ncases-per-day at first stay-at-home order") +
+  scale_colour_manual(name = "Average daily \ncases April 2020 \nto March 2021", values = c("FALSE" = "orangered3", "TRUE" = "dodgerblue"), labels = c("more than 100", "100 or less")) +
+  scale_linetype_manual(name = "Average daily \ncases April 2020 \nto March 2021", values = c("FALSE" = "dashed", "TRUE" = "solid"), labels = c("more than 100", "100 or less")) +
+  theme_bw()
+
+# produce fig 3b
+fig3b <- ggplot(data = filter(fig_2_data, under_100==TRUE), mapping = aes(x = Date, y = relative_cases, color = under_100)) +
+  geom_point(show.legend=FALSE) +
+  scale_y_log10(labels = c("0.01", "0.1", "1", "10", "100", "1000"), breaks = c(0.01, 0.1, 1, 10, 100, 1000), limits = c(0.0035, 800)) +
+  geom_text(data = filter(fig_2_data, under_100==TRUE, Date == last(Date)), aes(label=CountryCode),hjust=0, vjust=1, show.legend=FALSE, nudge_x = -25, nudge_y = 0.2) +
+  geom_line(aes(group = CountryCode, linetype = under_100), alpha = 0.4, size = 0.8, show.legend=FALSE) +
+  #  labs(title = "(b)",
+  #       y = "Cases-per-day, relative to \ncases-per-day at first stay-at-home order",
+  #       color = "Average daily cases since April 2020") +
+  ylab(NULL) +
+  scale_colour_manual(name = "Average daily \ncases April 2020 \nto March 2021", values = c("FALSE" = "orangered3", "TRUE" = "dodgerblue"), labels = c("more than 100", "100 or less")) +
+  scale_linetype_manual(name = "Average daily \ncases April 2020 \nto March 2021", values = c("FALSE" = "dashed", "TRUE" = "solid"), labels = c("more than 100", "100 or less")) +
+  theme_bw()
+
+plot_grid(fig3a, fig3b, labels = c("(a)", "(b)"), label_size = 10, rel_widths = c(1.1, 1))
